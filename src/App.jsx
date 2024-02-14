a8a9384476a3ed754687cc7b58b064734ad575b6
Anouar Touati <marouan1996@hotmail.fr>
2024-02-14 12:30:18 -0500

    talking with backend 100% works, can create products now

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Header } from './components/Header'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Header } from "./components/Header";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [products,setProducts] = useState({})
+  async function setUpAxios() {
+    axios.defaults.withCredentials = true;
+    await axios.get("http://localhost:80/sanctum/csrf-cookie");
+    axios.defaults.withXSRFToken = true;
+    await axios.post("http://localhost:80/api/login", {
+      email: "test@example.com",
+      password: "password",
+    });
+    const data1 = await axios.get("http://localhost:80/api/test");
+    console.log(data1);
+    const result = await axios.post("http://localhost:80/api/cart/products",{product_id:1})
+    console.log(result)
+    const data = await axios.get("http://localhost:80/api/cart/products")
+    console.log(data) 
+  }
+  useEffect(() => {
+    setUpAxios();
+  }, []);
   return (
     <>
-     <Header />
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,8 @@
-import axios from 'axios'
+
 import React, { useEffect } from 'react'
 
 export const Header = () => {
-    useEffect(()=>{
-        axios.defaults.withCredentials=true;
-        axios.get('http://localhost:80/sanctum/csrf-cookie').then(()=>{
-          axios.defaults.withXSRFToken=true;
-            axios.post('http://localhost:80/api/login',{
-              email:'test@example.com',
-              password:'password'
-            }).then((data)=>{
-              axios.get('http://localhost:80/api/test').then((data)=>{
-                console.log(data)
-            })
-                })
-         
-        })
-     
-    },[])
+ 
   return (
     <div>Header</div>
   )
